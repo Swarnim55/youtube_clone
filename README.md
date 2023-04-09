d26ccef37d2ae7e2d4a40ea854098e42107d1eb8
Swarnim Paudyal <swarnimpaudyal55@gmail.com>
2023-04-09 09:22:25 +0545

    added channel detail

--- README.md
+++ README.md
@@ -7,3 +7,4 @@
 5. Created Video Feed Component and consumed youtube video API from Rapid API with axios.
 6. Created and styled video cards, rendered videos based on category.
 7. Created channel cards, consumed and passed channel details to the component file.
+8. Created channel detail page
--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -1,7 +1,43 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { Box } from '@mui/material';
+import { Videos, ChannelCard } from './';
+import { fetchFromAPI } from './utils/fetchFromAPI';
 
 const ChannelDetail = () => {
-  return <div>ChannelDetail</div>;
+  const [channelDetail, setChannelDetail] = useState(null);
+  const [videos, setVideos] = useState([]);
+  const { id } = useParams();
+
+  useEffect(() => {
+    fetchFromAPI(`channels?part=snippet&id=${id}`).then((data) =>
+      setChannelDetail(data?.items[0])
+    );
+    fetchFromAPI(`search?channelId=${id}&part=snippet&order=date`).then(
+      (data) => setVideos(data?.items)
+    );
+  }, [id]);
+  return (
+    <Box minHeight="95vh">
+      <Box
+        style={{
+          background:
+            'linear-gradient(90deg, rgba(242,1,27,1) 0%, rgba(190,1,43,1) 17%, rgba(57,8,8,1) 63%)',
+          zIndex: 10,
+          height: '300px',
+        }}
+      />
+      <ChannelCard
+        channelDetail={channelDetail}
+        marginTop="-110px"
+      ></ChannelCard>
+
+      <Box display="flex" p="2">
+        <Box sx={{ mr: { sm: '100px' } }} />
+        <Videos videos={videos}></Videos>
+      </Box>
+    </Box>
+  );
 };
 
 export default ChannelDetail;
